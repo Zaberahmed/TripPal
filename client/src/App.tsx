71048608a5b50a75232701ce26d11c0518c9a682
Zaber Ahmed <zaber0305@gmail.com>
2023-09-02 20:30:09 +0600

    Misc: structuring dilemma

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/login/login.page';
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route
-					path="/login"
-					element={<LoginPage />}></Route>
+				<Route path="/login"></Route>
 			</Routes>
 		</Router>
 	);
