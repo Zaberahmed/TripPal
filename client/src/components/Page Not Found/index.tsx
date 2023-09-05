e145fbacfdb5b56d8e5072e1c580d3c72f97c393
Zaber Ahmed <zaber0305@gmail.com>
2023-09-05 13:12:41 +0600

    Modify: Page not found styles

--- client/src/components/Page Not Found/index.tsx
+++ client/src/components/Page Not Found/index.tsx
@@ -1,9 +1,29 @@
-import { Center, Heading } from '@chakra-ui/react';
+import { Center, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import pageNotFound from './../../assets/page-not-found.svg';
 
 const PageNotFound = () => {
 	return (
-		<Center mt={'10rem'}>
-			<Heading>Page Not Found !</Heading>
+		<Center mt={'8rem'}>
+			<Flex
+				direction={'column'}
+				alignItems={'center'}
+				gap={'1rem'}>
+				<Heading
+					fontFamily={'Dancing Script'}
+					fontWeight={'700'}
+					fontSize={'3rem'}>
+					Error
+				</Heading>
+				<Image
+					src={pageNotFound}
+					boxSize={'12rem'}></Image>
+				<Text
+					fontFamily={'Dancing Script'}
+					fontWeight={'700'}
+					fontSize={'3rem'}>
+					Page Not Found
+				</Text>
+			</Flex>
 		</Center>
 	);
 };
