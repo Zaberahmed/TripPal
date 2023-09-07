c58a5a0d0862158de667eaacec8ba7660bcd2a08
Zaber Ahmed <zaber0305@gmail.com>
2023-09-08 03:29:00 +0600

    Add: Flight filter

--- client/src/components/Flight Filter Button/index.tsx
+++ client/src/components/Flight Filter Button/index.tsx
@@ -1,5 +1,28 @@
-const FlightFilterButton = () => {
-	return <div>FlightFilterButton</div>;
+import { Button, Text } from '@chakra-ui/react';
+
+type FilterButton = {
+	text: string;
+};
+
+const FlightFilterButton = ({ text }: FilterButton) => {
+	return (
+		<Button
+			_selected={{ bg: 'actionPrimary', color: 'primary', cursor: 'pointer', transition: 'all .2s ease-out' }}
+			h={'3rem'}
+			w={'33.33333%'}
+			p={'.25rem .5rem '}
+			bg={'inherit'}
+			borderRadius={'none'}
+			borderRight={'1px solid #e5e5ea'}
+			_last={{ borderRight: 'none' }}
+			transition={'all .2s ease-in'}>
+			<Text
+				whiteSpace={'break-spaces'}
+				textAlign={'center'}>
+				{text}
+			</Text>
+		</Button>
+	);
 };
 
 export default FlightFilterButton;
--- client/src/pages/Flight/index.tsx
+++ client/src/pages/Flight/index.tsx
@@ -1,9 +1,21 @@
+import { Center, Flex } from '@chakra-ui/react';
 import TripInfoBox from '../../components/Trip Info Box';
+import FlightFilterButton from '../../components/Flight Filter Button';
 
 const FlightPage = () => {
 	return (
 		<>
 			<TripInfoBox />
+			<Center>
+				<Flex
+					bg={'#fafafa'}
+					mt={'2rem'}
+					w={'20rem'}>
+					<FlightFilterButton text={'Earliest Flights'} />
+					<FlightFilterButton text={'Cheapest Flights'} />
+					<FlightFilterButton text={'Fastest Flights'} />
+				</Flex>
+			</Center>
 		</>
 	);
 };
